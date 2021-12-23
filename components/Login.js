99f4e356b968575a60b15022f1538a3cbb51c4c5
marksikaundi <emmanuelmark117@gmail.com>
2021-12-24 01:00:16 +0200

    web 3.0

---
+++ components/Login.js
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import { useMoralis } from "react-moralis"
+
+
+function Login() {
+    const { authenticated } = useMoralis();
+
+    return (
+        <div className="bg-black relative">
+            <h1>Hi this is Login Screen</h1>
+            <div className="flex flex-col absolute z-50 h-4/6 w-full items-center justify-center space-y-4">
+                {/* papafam logo */}
+                <Image className="object-cover rounded-full" src="https://links.papareact.com/3pi" height={200} width={200} />
+
+
+                {/* login button */}
+                <button className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse">Login To Metaverse</button>
+
+
+            </div>
+
+            <div className="w-full h-screen">
+                {/* background Image */}
+                <Image src="https://links.papareact.com/55n" layout="fill" objectFit="cover" />
+            </div>
+        </div>
+    )
+}
+
+export default Login
