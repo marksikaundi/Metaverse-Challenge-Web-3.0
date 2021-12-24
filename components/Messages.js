a9323a2944975401af1fcc21c5aa50b5ed17e01c
marksikaundi <emmanuelmark117@gmail.com>
2021-12-24 21:01:48 +0200

    bug fixed

--- components/Messages.js
+++ components/Messages.js
@@ -1,7 +1,26 @@
+import{ ByMoralis, useMoralis, useMoralisQuery } from "react-moralis";
+import SendMessage from "./SendMessage";
+
 function Messages() {
+    const { user } = useMoralis();
+
     return (
-        <div>
+        <div className="pb-56">
+            <div className="my-5">
+                <ByMoralis variant="dark" style={{ marginLeft:'auto', marginRight:'auto' }} />
+            </div>
+            <div>
+
+            </div>
+
+            <div className="flex justify-center">
+                <SendMessage />
+            </div>
+
             
+            <div className="text-center text-gray-400 mt-5">
+                <p>You 're up to date {user.getUsername()}!🎉 </p>
+            </div>
         </div>
     )
 }
